a69ba3771bf4d472e8651d0f42b452fde18ccaae
Nico <104150729+whoisnico@users.noreply.github.com>
2024-11-11 14:42:31 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,15 +1,12 @@
 // content.js
 
-// Beispiel: Den Wert des Elements mit der Klasse .price auslesen
-const priceElement = document.querySelector('.price');
-if (priceElement) {
-  const price = priceElement.innerText;
-  console.log('Der Preis ist: ', price);
+// Finde das erste Element mit der Klasse 'syllable'
+const syllableElement = document.querySelector('.syllable');
 
-  // Hier kannst du den Wert an das Popup oder an den Hintergrund-Skripten weitergeben
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'getPrice') {
-      sendResponse({ price: price });
-    }
-  });
+// Überprüfe, ob das Element existiert und lies den Text aus
+if (syllableElement) {
+  const syllableText = syllableElement.innerText;
+  console.log('Gefundener Text:', syllableText);
+} else {
+  console.log('Kein Element mit der Klasse "syllable" gefunden.');
 }
